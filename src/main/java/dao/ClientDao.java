d6fa734914020a201b1c67c0f844c8a3a7ce8ea5
andrea cuadra <acuadrap@uamv.edu.ni>
2024-10-08 18:45:45 -0600

    complete la clase clientDao

--- src/main/java/dao/ClientDao.java
+++ src/main/java/dao/ClientDao.java
@@ -1,6 +1,7 @@
 package dao;
 
 import interfaces.IClient;
+import jakarta.persistence.EntityManager;
 import models.Client;
 
 import java.util.List;
@@ -8,26 +9,39 @@ import java.util.List;
 public class ClientDao implements IClient {
     @Override
     public void save(Client client) {
-        
+        EntityManager em = EntityManagerAdmin.getInstance();
+        em.getTransaction().begin();
+        em.persist(client);
+        em.getTransaction().commit();
     }
 
     @Override
     public Client getClient(Long id) {
-        return null;
+        EntityManager em = EntityManagerAdmin.getInstance();
+        return em.find(Client.class, id);
     }
 
     @Override
     public void delete(Client client) {
-
+        EntityManager em = EntityManagerAdmin.getInstance();
+        em.getTransaction().begin();
+        Client clientToDelete = em.find(Client.class, client.getId());
+        if (clientToDelete != null) {
+            em.remove(clientToDelete);
+        }
     }
 
     @Override
     public void update(Client client) {
-
+        EntityManager em = EntityManagerAdmin.getInstance();
+        em.getTransaction().begin();
+        em.merge(client);
+        em.getTransaction().commit();
     }
 
     @Override
     public List<Client> getAllClients() {
-        return List.of();
+        EntityManager em = EntityManagerAdmin.getInstance();
+        return em.createQuery("from Client", Client.class).getResultList();
     }
 }
